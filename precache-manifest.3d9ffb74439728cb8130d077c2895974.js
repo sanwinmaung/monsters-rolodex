caa7c2e300a82369693c77b9e9e56a6233be0888
sanwinmaung <sanwinmaung.dev@gmail.com>
2019-12-04 00:01:09 +0630

    Updates

---
+++ precache-manifest.3d9ffb74439728cb8130d077c2895974.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ee86c492e83877c777c796570b76a3f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dd7e71af2579493f2049",
+    "url": "/monsters-rolodex/static/css/main.3a56ab66.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "dd7e71af2579493f2049",
+    "url": "/monsters-rolodex/static/js/main.863285c6.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
